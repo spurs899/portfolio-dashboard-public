028af687e912fe599e812293788e73e67a8129e4
spurs899 <spurs899@gmail.com>
2026-01-03 15:23:46 +1300

    Adding initial scaffold of solution and projects.

---
+++ PortfolioManager.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PortfolioManager.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ PortfolioManager.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace PortfolioManager.Core;
+
+public class Class1
+{
+
+}
