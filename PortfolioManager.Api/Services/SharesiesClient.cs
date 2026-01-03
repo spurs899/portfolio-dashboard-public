c28cc0a13b2c654c31134cadb54e74db9792f4f5
spurs899 <spurs899@gmail.com>
2026-01-03 23:44:15 +1300

    Remove base address.

--- PortfolioManager.Api/Services/SharesiesClient.cs
+++ PortfolioManager.Api/Services/SharesiesClient.cs
@@ -13,7 +13,6 @@ public class SharesiesClient : ISharesiesClient
     public SharesiesClient(HttpClient httpClient)
     {
         _httpClient = httpClient;
-        //_httpClient.BaseAddress = new Uri(BaseUrl);
         _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36");
     }
 
